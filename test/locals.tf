a89d61f1d09f24d72d026d6afcfb1710676f012a
ymz-note <rentaro.y.813@gmail.com>
2024-09-22 19:32:59 +0900

    20240922 0001

--- test/locals.tf
+++ test/locals.tf
@@ -1,4 +1,6 @@
 locals {
   compartment_id = "ocid1.compartment.oc1..aaaaaaaa3bid7ry3jiwrljzxxr4tpeldg3aysq2vvylrxjr5mnqas23s2nda"
-  
+  vcn_cidr = "192.168.0.0/16"
+  vcn_subnet_cidr = "192.168.0.0/24"
+  dns_label = "vcn20240922"
 }
